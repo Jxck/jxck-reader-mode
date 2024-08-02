f4e964692b9108b12de0860e605dd4a70e259bb4
Jxck <jxck@jxck.io>
2024-08-03 03:19:33 +0900

    export copy_link

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 import { translate } from "./translate.js";
+import { copy_link } from "./copy_link.js";
 
 const MODE = {
   CLEAR: "clear-translate",
@@ -49,36 +50,3 @@ chrome.commands.onCommand.addListener(async (command) => {
     await copy_link();
   }
 });
-
-async function copy_link() {
-  // get current tab
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  const [{ result }] = await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: async () => {
-      const canonical = document.querySelector("link[rel=canonical]")?.href;
-      const url = canonical || location.href;
-      const title = document.title.trim();
-
-      const html = `<ul>
-                      <li>${title}
-                        <ul>
-                          <li><a href="${url}">${url}</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  `;
-      const text = `${title}\n\t${url}`;
-
-      await navigator.clipboard.write([
-        new ClipboardItem({
-          "text/html": new Blob([html], { type: "text/html" }),
-          "text/plain": new Blob([text], { type: "text/plain" }),
-        }),
-      ]);
-    },
-  });
-}
