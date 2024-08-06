908f38e98a67f72cbcad79ff2a06e258ec2bd784
Jxck <jxck@jxck.io>
2024-08-07 00:43:07 +0900

    add hatena.js

---
+++ user_scripts/hatena.js
@@ -0,0 +1,35 @@
+(function () {
+  "use strict";
+
+  const blacklist = ["anond.hatelabo.jp", "togetter.com", "b.hatena.ne.jp"];
+
+  // 俺のはてブ
+  if (location.href.startsWith("https://hatebu.brdr.jp")) {
+    Array.from(document.querySelectorAll("a[target='_blank']"))
+      .filter((e) => {
+        const href = new URL(e.href);
+        return blacklist.includes(href.host);
+      })
+      .forEach((e) => {
+        e.parentElement.remove();
+      });
+
+    document.querySelectorAll("div.entry-info").forEach((e) => e.remove());
+  }
+
+  // はてなフィルター
+  if (location.href.startsWith("https://hatenafilter.com")) {
+    Array.from(
+      document.querySelectorAll("div.entry > div.title > a[target='_blank']"),
+    )
+      .filter((e) => {
+        const href = new URL(e.href);
+        return blacklist.includes(href.host);
+      })
+      .forEach((e) => {
+        console.log(e);
+        e.parentElement.parentElement.remove();
+      });
+    document.querySelectorAll("span.bookmark").forEach((e) => e.remove());
+  }
+})();
