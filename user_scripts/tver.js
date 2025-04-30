2774e490f111bb448990b0faa0cdd5a95317f6d6
Jxck <jxck@jxck.io>
2025-05-01 05:02:20 +0900

    tver

---
+++ user_scripts/tver.js
@@ -0,0 +1,6 @@
+(function () {
+  "use strict";
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
+  EventTarget.prototype.on = EventTarget.prototype.addEventListener;
+})();
